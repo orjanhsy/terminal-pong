a9fa1c68219271bd0edfa41d4d12a97d9700375d
oerjan <orjanhsy@gmail.com>
2024-08-02 17:42:04 +0200

    initial code for bat
    using tcell

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
-module github.com/orjanhsy/terminal-pong
+module snake
 
-go 1.22.5
+go 1.22.2
+
+require (
+	github.com/gdamore/tcell v1.4.0
+	golang.org/x/term v0.22.0
+)
+
+require (
+	github.com/gdamore/encoding v1.0.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
---
+++ term.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"golang.org/x/term"
+)
+
+
+var screen = bufio.NewWriter(os.Stdout)
+
+func resetTextElements() {
+	fmt.Fprint(screen, "\033[0m")
+}
+
+func hideCursor() {
+	fmt.Fprint(screen, "\033[?25l")
+}
+
+func showCursor() {
+	fmt.Fprint(screen, "\033[?25h")
+}
+
+func moveCursor(pos [2]int) {
+	fmt.Fprintf(screen, "\033[%d;%dH", pos[1], pos[0])
+}
+
+func clear() {
+	fmt.Fprint(screen, "\033[2J")
+}
+
+func draw(str string) {
+	fmt.Fprint(screen, str)
+}
+
+func render() {
+	screen.Flush()
+}
+
+func getSize() (int, int) {
+	width, height, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		panic(err)
+	}
+
+	return width, height
+}
